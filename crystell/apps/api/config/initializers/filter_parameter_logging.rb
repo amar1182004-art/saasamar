d3ec1aef928163d1e38a6574f4c8326aca141388
amar1182004-art <amar1182004@gmail.com>
2026-08-13 02:46:49 +0300

    Filter credential derivatives from Rails logs

--- crystell/apps/api/config/initializers/filter_parameter_logging.rb
+++ crystell/apps/api/config/initializers/filter_parameter_logging.rb
@@ -1,13 +1,17 @@
 Rails.application.config.filter_parameters += %i[
   password
+  password_digest
   password_confirmation
   token
+  token_digest
   access_token
   refresh_token
   challenge_token
   recovery_code
+  recovery_code_digests
   code
   authorization
   secret
+  encrypted_secret
   api_key
 ]
