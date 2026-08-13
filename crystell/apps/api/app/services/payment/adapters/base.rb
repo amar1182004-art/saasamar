68736780da3b69724e396b74c2fc797320fb1a34
amar1182004-art <amar1182004@gmail.com>
2026-08-13 09:55:02 +0300

    Add payment adapter contract

---
+++ crystell/apps/api/app/services/payment/adapters/base.rb
@@ -0,0 +1,28 @@
+module Payment
+  module Adapters
+    class Base
+      IntentResult = Data.define(:provider_intent_id, :status, :checkout_url, :provider_status, :metadata)
+      WebhookResult = Data.define(:provider_event_id, :event_type, :provider_intent_id, :status, :amount_cents, :currency, :provider_transaction_id, :metadata)
+
+      def initialize(account:)
+        @account = account
+      end
+
+      def create_intent(payment_intent:)
+        raise NotImplementedError
+      end
+
+      def verify_webhook!(raw_body:, headers:)
+        raise NotImplementedError
+      end
+
+      def parse_webhook(raw_body:)
+        raise NotImplementedError
+      end
+
+      private
+
+      attr_reader :account
+    end
+  end
+end
