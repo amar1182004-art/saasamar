804292fa7f027c7a11f874d27424e869e5f7c8c3
amar1182004-art <amar1182004@gmail.com>
2026-08-13 10:40:08 +0300

    Apply selected shipping quote to checkout totals

---
+++ crystell/apps/api/app/services/shipping/quote_selector.rb
@@ -0,0 +1,34 @@
+module Shipping
+  class QuoteSelector
+    class MissingTenantContextError < StandardError; end
+    class InvalidQuoteError < StandardError; end
+
+    def self.call(checkout_session_id:, shipping_rate_quote_id:, shipping_address:, now: Time.current)
+      raise MissingTenantContextError, "tenant context is required" if Current.tenant_id.blank?
+
+      ApplicationRecord.transaction(requires_new: true) do
+        checkout = CheckoutSession.lock.find(checkout_session_id)
+        raise InvalidQuoteError, "checkout cannot change shipping in its current state" unless %w[open inventory_reserved].include?(checkout.status)
+        raise InvalidQuoteError, "checkout has expired" if checkout.expires_at <= now
+
+        quote = ShippingRateQuote.find(shipping_rate_quote_id)
+        raise InvalidQuoteError, "shipping quote belongs to another checkout" unless quote.checkout_session_id == checkout.id
+        raise InvalidQuoteError, "shipping quote has expired" if quote.expires_at <= now
+        raise InvalidQuoteError, "shipping quote currency mismatch" unless quote.currency == checkout.currency
+
+        address = shipping_address.respond_to?(:to_h) ? shipping_address.to_h.stringify_keys : {}
+        raise InvalidQuoteError, "shipping address country_code is required" if address["country_code"].blank?
+
+        new_total = checkout.subtotal_cents - checkout.discount_cents + quote.amount_cents + checkout.tax_cents
+        checkout.update!(
+          selected_shipping_rate_quote_id: quote.id,
+          shipping_cents: quote.amount_cents,
+          total_cents: new_total,
+          shipping_address: address
+        )
+
+        checkout
+      end
+    end
+  end
+end
